77a481ba0e7750dc5a024a149b544d5b93e22db3
Lays Rodrigues <laysrodriguessilva@gmail.com>
2022-12-19 10:22:48 -0300

    fix: use web instead of api
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function listener(e) {
   e.preventDefault();
   let number = document.getElementById("phoneNumber").value;
   let cleanNumber = number.replace(/\D+/g, "");
-  let formatedUrl = `https://api.whatsapp.com/send?phone=55${cleanNumber}`;
+  let formatedUrl = `https://web.whatsapp.com/send?phone=55${cleanNumber}`;
 
   document.getElementById("result").style.display = "block";
   document.getElementById("url").value = document.getElementById(
